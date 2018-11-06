d7befe531a13c502fe585d0c1a72671b9688a9bf
Creep53 <creepminecraft53@gmail.com>
2018-11-06 18:16:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,25 +28,8 @@ bot.on('message', msg => {
     if (msg.content === "bonjour"){
         msg.reply("Heureux de te revoir parmis nous.")
     }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if(message.content === prefix + "fun"){
-        var fun_embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setTitle(`:tools: Voici mes commandes amusantes !`)
-      .setThumbnail(message.author.avatarURL)
-      .addField("Bonjour","il y a plein de petit mot caché comme ça trouve les pour discuté")
-      .addField("Cp stats", "Le bot vous envoie des informations sur votre profil !")
-      .addField("Cp info", "Donne des indormations sur le bot et le serveur !")
-	.addField("Cp cat", "Affiche des gifs de chat")
-	  .addField("Cp narnia", "Affiche des gifs de narnia")
-	  .addField("Cp wtf", "Affiche des gifs wtf")
-	  .addField("Cp Yuki", "Affiche des images de (attention c'est dure) Yuki")
-      .setFooter("Commande Fun - By Creep_1")
-      .setTimestamp()
-      message.channel.send(fun_embed);
-    }
+   
+
 });
 
 bot.login(token); //a garder en version heroku
