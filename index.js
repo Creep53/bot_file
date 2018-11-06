a203fd5e087ed095ca7c467916ac018a99df2cac
Creep53 <creepminecraft53@gmail.com>
2018-11-06 18:34:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.on('message', async message => {
       .setTimestamp()
       message.channel.send(fun_embed);
     }
-    //message pour afficher les commandes dans le fun
+    //message pour afficher les commandes du fun
 if(message.content.startsWith(prefix + "cat")){
         var cat = [
 			"https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
@@ -132,7 +132,105 @@ if(message.content.startsWith(prefix + "cat")){
 	.setFooter("WTF WTF WTF WTF WTF")
 	message.channel.send(aleatoire_embed);
 	}
+ //message pour afficher le mod
+	 if(message.content === prefix + "mod") {
+      var mod_embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`:tools: Voici mes commandes modérations !`)
+      .setThumbnail(message.author.avatarURL)
+      .addField("Cp kick <@user>", "Kick l'utilisateur !")
+      .addField("Cp ban <@user>", "Ban l'utilisateur !")
+      .addField("Cp clear nombre", "Supprime le nombre de messages indiqué")
+      .addField("Cp mute <@user>", "Mute l'utilisateur mentionné")
+      .addField("Cp unmute <@user>", "Unmute l'utilisateur mentionné")
+      .setFooter("Commande modération - By Creep_1")
+      .setTimestamp()
+      message.channel.send(mod_embed);
+    }
+	//message pour afficher les commandes du mod
+	 if(message.content.startsWith(prefix + "kick")){
+        if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission!");
     
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez metionner un utilisaeur")
+        }
+        var kick = message.guild.member(message.mentions.users.first());
+        if(!kick) {
+            return message.channel.send("Je ne sais pas si l'utilisateur existe :/")
+        }
+    
+        if(message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la permission pour kick");
+        }
+    
+        kick.kick().then(member => {
+            message.channel.send(`${member.user.username} est kick pas ${message.author.username}`);
+        });
+    }
+
+    if(message.content.startsWith(prefix + "ban")) {
+        if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.send("Vous n'avez pas la perission");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez mentionner un utilisateur");
+        }
+
+        var ban = message.guild.member(message.mentions.users.first());
+        if(!ban) {
+            return message.channel.send("Je ne sais pas si l'utilisateur existe");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la permission pour ban");
+        }
+        ban.ban().then(member => {
+            message.channel.send(`${member.user.username} est ban par ${message.author.username} !`)
+        });   
+    }
+	 if(message.content.startsWith(prefix + "clear")) {
+        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGE")) return message.channel.send("Vous n'avez pas la permission !");
+
+        let args = message.content.split(" ").slice(1);
+
+        if(!args[0]) return message.channel.send("Tu dois préciser un nombre de messages à supprimer !")
+        message.channel.bulkDelete(args[0]).then(() => {
+            message.channel.send(`${args[0]} messages ont été supprimés !`);
+        });
+    }
+
+    if(message.content.startsWith(prefix + "mute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send('Vous devez mentionner un utilisateur !');
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: false}).then(member => {
+            message.channel.send(`${mute.user.username} est mute !`);
+        });
+    }
+
+    if(message.content.startsWith(prefix + "unmute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send('Vous devez mentionner un utilisateur !');
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: true}).then(member => {
+            message.channel.send(`${mute.user.username} n'est plus mute !`);
 });
 
 bot.login(token); //a garder en version heroku
