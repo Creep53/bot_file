aab28f8d18133e497b5415c9ddac7cfea3fd6a80
Creep53 <creepminecraft53@gmail.com>
2018-11-06 18:01:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,25 +24,25 @@ bot.on('message', function (message){
     }
 });
 
-
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
+bot.on('message', async message => { 
+    
+        if (msg.content === "bonjour"){
         msg.reply("Heureux de te revoir parmis nous.")
     }
-    if (msg.content.match(/salut/i)) {
+        if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
     }
-    if (msg.content === prefix + "help"){
-        var aide_embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setTitle(`:robot: Voici mes catégories d'aide !`)
-      .setDescription(`Voici mes commandes disponible :`)
-      .addField(":tools: Modération", "Fais `Cp mod` pour voir mes commandes de modération !")
-      .addField(":tada: Fun", "Fais `Cp fun` pour voir mes commandes d'animation !")
-      .setFooter("Menu d'aide - By Creep_1")
-      .setTimestamp()
-      message.channel.send(aide_embed);        
-        console.log("Une personne a demandé pour aller sur ton site.")
+        if (msg.content === prefix + "help"){
+             var aide_embed = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle(`:robot: Voici mes catégories d'aide !`)
+            .setDescription(`Voici mes commandes disponible :`)
+            .addField(":tools: Modération", "Fais `Cp mod` pour voir mes commandes de modération !")
+            .addField(":tada: Fun", "Fais `Cp fun` pour voir mes commandes d'animation !")
+            .setFooter("Menu d'aide - By Creep_1")
+            .setTimestamp()
+            message.channel.send(aide_embed);        
+            console.log("Une personne a demandé pour aller sur ton site.")
     }
 
 });
