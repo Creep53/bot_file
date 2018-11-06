cc96426812e5fb44c55c06091798f0dbbdfac611
Creep53 <creepminecraft53@gmail.com>
2018-11-06 18:05:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,83 @@ bot.on('message', async message => {
         if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
     }
-        if (msg.content === prefix + "help"){
+    if(message.content.startsWith(prefix + "cat")){
+        var cat = [
+			"https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+			"https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif",
+			"https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif"
+		];
+    var gif = cat[Math.floor(Math.random() * cat.length)]; 
+	
+	var cat_embed = new Discord.RichEmbed()
+	 .setColor('RANDOM')
+	 .setTitle(':cat: Chat:')
+	 .setImage(gif)
+	.setFooter('CHAT CHAT CHAT CHAT')
+	message.channel.send(cat_embed);
+	}
+	
+	if(message.content.startsWith(prefix + "Yuki")){
+        var Yuki = [
+			"https://cdn.discordapp.com/attachments/417516189619585024/507993319767408660/Snapchat-66221107.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507993515943264286/Snapchat-1883413935.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507993646373535746/Snapchat-410157663.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507993757182853130/Snapchat-1423020034.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507994773336883200/IMG_20180107_122344.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507995280990273549/IMG_20180416_194429.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507996560240541698/IMG_20180703_101443.jpg",
+			"https://cdn.discordapp.com/attachments/417516189619585024/507996898221883404/IMG_20180714_192659_003.jpg"
+		
+
+		];
+    var gif = Yuki[Math.floor(Math.random() * Yuki.length)]; 
+	
+	var Yuki_embed = new Discord.RichEmbed()
+	 .setColor('RANDOM')
+	 .setTitle(':cat: Chat:')
+	 .setImage(gif)
+	.setFooter('CHAT CHAT CHAT CHAT')
+	message.channel.send(Yuki_embed);
+	}
+	
+	if(message.content.startsWith(prefix + "narnia")){
+        var narnia = [
+			"https://media.giphy.com/media/ytL3y83qPtic8/giphy.gif",
+			"https://media.giphy.com/media/4PuHxplO8XuyA/giphy.gif",
+			"https://media.giphy.com/media/mEuI7NjX90BNu/giphy.gif"
+			
+				];
+    var gif = narnia[Math.floor(Math.random() * narnia.length)]; 
+	
+	var narnia_embed = new Discord.RichEmbed()
+	 .setColor('RANDOM')
+	 .setTitle('Narnia')
+	 .setImage(gif)
+	.setFooter("Narnia Narnia Narnia Narnia")
+	message.channel.send(narnia_embed);
+	}
+		if(message.content.startsWith(prefix + "wtf")){
+        var aleatoire = [
+			"https://media.giphy.com/media/l46Cuu0O6xJ42z66I/giphy.gif",
+			"https://media.giphy.com/media/13BwjdpxACoBPO/giphy.gif",
+			"https://media.giphy.com/media/ToMjGpPbMMFCWHrqk6Y/giphy.gif",
+			"https://media.giphy.com/media/AIgTmKOesNSWA/giphy.gif",
+			"https://media.giphy.com/media/QxZQ7dSV4j6GQ/giphy.gif",
+			"https://media.giphy.com/media/IYqe9gVpujtWo/giphy.gif",
+			"https://media.giphy.com/media/13nhYQqKEPQAyA/giphy.gif",
+			"https://media.giphy.com/media/o8GQT5Ipi4spy/giphy.gif"
+	
+				];
+    var gif = aleatoire[Math.floor(Math.random() * aleatoire.length)]; 
+	
+	var aleatoire_embed = new Discord.RichEmbed()
+	 .setColor('RANDOM')
+	 .setTitle('WTF')
+	 .setImage(gif)
+	.setFooter("WTF WTF WTF WTF WTF")
+	message.channel.send(aleatoire_embed);
+	}    
+    if (msg.content === prefix + "help"){
              var aide_embed = new Discord.RichEmbed()
             .setColor('RANDOM')
             .setTitle(`:robot: Voici mes catégories d'aide !`)
