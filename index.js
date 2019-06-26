168214338324a91e6125afdc80269d05565b3f53
Creep53 <creepminecraft53@gmail.com>
2019-06-26 12:38:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ bot.on('ready', function () {
 });
 
 bot.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const serverQueue = queue.get(message.guild.id);
+
+	if (message.content.startsWith(prefix+"play")) {
+		execute(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(prefix+"skip")) {
+		skip(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(prefix+"stop")) {
+		stop(message, serverQueue);
+		return;
+	} else {
+		message.channel.send('You need to enter a valid command!')
+	}
     //discution avec le bot 	
 	if (message.content === "Bonjour"){
         message.reply("Heureux de te revoir parmis nous.")
@@ -297,26 +314,6 @@ if (message.content.startsWith(prefix + "clear")) {
                 }, function(err){message.channel.send("Pas de assez de permission pour executer la commande")})                        
         }
     }
-});
-
-bot.on('message', async message => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-
-	const serverQueue = queue.get(message.guild.id);
-
-	if (message.content.startsWith(prefix+"play")) {
-		execute(message, serverQueue);
-		return;
-	} else if (message.content.startsWith(prefix+"skip")) {
-		skip(message, serverQueue);
-		return;
-	} else if (message.content.startsWith(prefix+"stop")) {
-		stop(message, serverQueue);
-		return;
-	} else {
-		message.channel.send('You need to enter a valid command!')
-	}
 });
 
 async function execute(message, serverQueue) {
